cb3e5910fca0494b1f6f8ea1ee7e8e54d9cb3f1d
Olay G <olayg@icloud.com>
2019-11-16 13:44:17 -0500

    updates

--- app/src/main/java/com/example/weatherwookotlin/repository/remote/RemoteConstants.kt
+++ app/src/main/java/com/example/weatherwookotlin/repository/remote/RemoteConstants.kt
@@ -3,5 +3,5 @@ package com.example.weatherwookotlin.repository.remote
 object RemoteConstants {
     const val BASE_URL = "https://api.darksky.net"
     const val WEATHER_PATH = "/forecast/{apiKey}/{longitude},{latitude}?exclude=[minutely,alerts,flags]"
-    const val API_KEY = "88089289b38af1fccc17e0fd5818ae00"
+    const val API_KEY = ""
 }
